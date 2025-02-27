b8d53f35d6ca80e9894e5061fdd6c36cef5e9425
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-02-27 23:51:25 +0530

    🎨 design(ui): add basic navbar, main, footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,111 @@
 import Image from "next/image";
-
+import "./style.css";
+import { brotliCompress } from "zlib";
+import { Instagram, Linkedin, MapPin, User } from "lucide-react";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <div className="containerMax">
+        <div className="nav">
+          <div className="logoContainer">
+            <div className="logo">NEXTBENCH</div>
+            <div className="location">
+              <MapPin
+                size={13}
+                color="#52525B"
+                style={{ marginBottom: "2.3px" }}
+              />
+              Dehradun
+            </div>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="midCont">
+            <div className="links">Admission</div>
+            <div className="links">Comparison</div>
+            <div className="links">Explore</div>
+          </div>
+
+          <div className="right">
+            <div className="button">Shortlist</div>
+            <div className="profile">
+              <User size={20} />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        {/*main start*/}
+        <div className="main">
+          <div className="textTop">Find <span>schools</span> that fits for your <br/>child.</div>
+          <div className="textBottom">Finding the right school shouldn't be hard. From K-12 to college to grad school, we make it School Search easy.</div>
+        </div>
+        {/*main end*/}
+
+        {/*Footer start*/}
+        <div className="footer">
+          <div className="footerStyle">
+            <div className="info">
+              <h1>NEXT BENCH</h1>
+              <br />
+              <h2>About Us</h2>
+              <br />
+              <p>Lorem Ipsum</p>
+            </div>
+
+            <div className="footerLinks">
+              <div className="company">
+                <h3>Company</h3>
+                <br />
+                <a href="#">About</a>
+                <a href="#">Admission Taker</a>
+                <a href="#">Comparison</a>
+                <a href="#">Shortlisted School</a>
+              </div>
+
+              <div className="help">
+                <h3>Help</h3>
+                <br />
+                <a href="#">Customer Support</a>
+                <a href="#">For teachers</a>
+                <a href="#">Terms and Condition</a>
+                <a href="#">Privacy Policy</a>
+              </div>
+
+              <div className="resources">
+                <h3>Resources</h3>
+                <br />
+                <a href="#">Schools</a>
+                <a href="#">College</a>
+                <a href="#">Universities</a>
+                <a href="#">Others</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="socialLinks">
+            <a href="#">
+              <Linkedin /> {/*LUCIDE REACT ICONS */}
+            </a>
+            <a href="#">
+              <Instagram /> {/*LUCIDE REACT ICONS */}x
+            </a>
+          </div>
+        </div>
+        {/*Footer end*/}
+        {/*containerMax end*/}
+        {/*TRASH AREA 
+
+        */}
+      </div>
+    </>
   );
 }
+
+//What extension are you using? mien are not working?
+//auto-complete tag,error lens,prettier code formatter.
+//ok
+
+// for  prettier code formatter, you can try to install it and press shift+alt+f to format the code
+//ok
+
+// CURRENTLY WE WILL MAKE ONLY HOME > COMING SOON PAGE > FOOTER
+//ok
