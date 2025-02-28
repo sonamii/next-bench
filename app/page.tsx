10a5fa4b28587b9729140cba4f037e42572b465a
Mehul Negi <mehulnegi2008@gmail.com>
2025-02-28 14:16:07 +0530

    🎨 design(coming-soon): add coming-soon section

--- app/page.tsx
+++ app/page.tsx
@@ -5,40 +5,17 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
+import {Navbar} from './../custom/Nav';
+import {Footer} from './../custom/footer';
 export default function Home() {
-  return (
-    <>
-      <div className="containerMax">
-        <div className="nav">
-          <div className="logoContainer">
-            <div className="logo">NEXT BENCH</div>
-            <div className="location">
-              <MapPin
-                size={13}
-                color="#52525B"
-                style={{ marginBottom: "4px" }}
-              />
-              Dehradun
-            </div>
-          </div>
-
-          <div className="midCont">
-            <div className="links">Admission Tracker</div>
-            <div className="links">Comparison</div>
-            <div className="links">Explore</div>
-          </div>
-
-          <div className="right">
-            <div className="button">Shortlist</div>
-
-            <div className="profile">
-              <User size={20} />
-            </div>
-          </div>
-        </div>
+  return (    
+  <>
+    <div className="containerMax">
+        
+      <Navbar/>
 
         {/*main start*/}
-        <section className="home" style={{width: '100%'}}>
+        <section className="mainSection" id="mainSection" style={{width: '100%'}}>
           <div className="main">
             <div className="containerMain">
               <div className="a">
@@ -141,99 +118,28 @@ export default function Home() {
           </div>
         </section>
 
-        <div className="footer">
-          {/* <div className="footerStyle">
-            <div className="info">
-              <h1>NEXT BENCH</h1>
-              <br />
-              <h2>About Us</h2>
-              <br />
-              <p>Lorem Ipsum</p>
-            </div>
+        <section className="comingSoonSection" id="comingSoonSection">
+          <div className="comingSoon">
+            <h1>Coming Soon...</h1>
 
-            <div className="footerLinks">
-              <div className="company">
-                <h3>Company</h3>
-                <br />
-                <Link href="#">About</Link>
-                <Link href="#">Admission Taker</Link>
-                <Link href="#">Comparison</Link>
-                <Link href="#">Shortlisted School</Link>
+            <div className="email">
+              <div className="emailHeading">
+                <p>Enter your email below to be notified as soon as our website is fully complete and ready to use!</p>
               </div>
-
-              <div className="help">
-                <h3>Help</h3>
-                <br />
-                <Link href="#">Customer Support</Link>
-                <Link href="#">For teachers</Link>
-                <Link href="#">Terms and Condition</Link>
-                <Link href="#">Privacy Policy</Link>
-              </div>
-
-              <div className="resources">
-                <h3>Resources</h3>
-                <br />
-                <Link href="#">Schools</Link>
-                <Link href="#">College</Link>
-                <Link href="#">Universities</Link>
-                <Link href="#">Others</Link>
-              </div>
-            </div>
-          </div> */}
-
-          {/* <div className="socialLinks">
-            <Link href="#">
-              <Linkedin />
-            </Link>
-            <Link href="#">
-              <Instagram />
-            </Link>
-          </div> */}
-
-          <div className="left">
-            <div className="top">
-              <h1>NEXT BENCH</h1>
-            </div>
-            <div className="bottom">
-              <h1>About us</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                dictum aliquet accumsan porta lectus ridiculus in mattis. Netus
-                sodales in volutpat ullamcorper amet adipiscing fermentum.
-              </p>
-            </div>
-          </div>
-          <div className="right">
-            <div className="item">
-              <h1>Product</h1>
-              <div className="p">
-                <Link href="/">About</Link>
-                <Link href="/">Admisison Tracker</Link>
-                <Link href="/">Comparison</Link>
-                <Link href="/">Shortlisted Schools</Link>
-              </div>
-            </div>
-            <div className="item">
-              <h1>Help</h1>
-              <div className="p">
-                <Link href="/">Customer Support</Link>
-                <Link href="/">For Teachers</Link>
-                <Link href="/">Terms and Conditions</Link>
-                <Link href="/">Privacy Policy</Link>
-              </div>
-            </div>
-            <div className="item">
-              <h1>Resource</h1>
-              <div className="p">
-                <Link href="/">School</Link>
-                <Link href="/">College</Link>
-                <Link href="/">Universities</Link>
-                <Link href="/">Others</Link>
+              <div className="emailInput">
+                <div className="emailTextBox">
+                  <Input type="email" placeholder="Email"></Input>
+                </div>
+                <div className="emailButton">
+                  <Button type="submit">Submit</Button>
+                </div>
               </div>
             </div>
           </div>
+        </section>
+
+<Footer/>
         </div>
-      </div>
     </>
   );
 }
---
+++ custom/footer.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+export function Footer() {
+
+return  (<><div className="footer">
+    <div className="left">
+      <div className="top">
+        <h1>NEXT BENCH</h1>
+      </div>
+      <div className="bottom">
+        <h1>About us</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          dictum aliquet accumsan porta lectus ridiculus in mattis. Netus
+          sodales in volutpat ullamcorper amet adipiscing fermentum.
+        </p>
+      </div>
+    </div>
+    <div className="right">
+      <div className="item">
+        <h1>Product</h1>
+        <div className="p">
+          <Link href="#comingSoonSection">About</Link>
+          <Link href="#comingSoonSection">Admisison Tracker</Link>
+          <Link href="#comingSoonSection">Comparison</Link>
+          <Link href="#comingSoonSection">Shortlisted Schools</Link>
+        </div>
+      </div>
+      <div className="item">
+        <h1>Help</h1>
+        <div className="p">
+          <Link href="#comingSoonSection">Customer Support</Link>
+          <Link href="#comingSoonSection">For Teachers</Link>
+          <Link href="#comingSoonSection">Terms and Conditions</Link>
+          <Link href="#comingSoonSection">Privacy Policy</Link>
+        </div>
+      </div>
+      <div className="item">
+        <h1>Resource</h1>
+        <div className="p">
+          <Link href="#comingSoonSection">School</Link>
+          <Link href="#comingSoonSection">College</Link>
+          <Link href="#comingSoonSection">Universities</Link>
+          <Link href="#comingSoonSection">Others</Link>
+        </div>
+      </div>
+    </div>
+  </div></>)
+
+}
