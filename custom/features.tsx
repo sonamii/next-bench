9a847cfd06365ac92bf12ce8c8331e4f9f8a0311
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-02-28 16:22:23 +0530

    🐛 fix(feature): add and fix feature section

--- custom/features.tsx
+++ custom/features.tsx
@@ -6,7 +6,29 @@ export function Features() {
   return (
     <>
       <section className="features" id="features">
-        
+        <div className="textTop">
+          What are you <span>looking</span> for?
+        </div>
+        <div className="textBottom">
+          Discover and connect with the best ones
+        </div>
+        <div className="cardContainer">
+          <div className="item">
+            <div className="text">Play <br/>School</div>
+          </div>
+          <div className="item">
+            {" "}
+            <div className="text">Boarding <br/>School</div>
+          </div>
+          <div className="item">
+            {" "}
+            <div className="text">Day <br/>Boarding</div>
+          </div>
+          <div className="item">
+            {" "}
+            <div className="text">Colleges</div>
+          </div>
+        </div>
       </section>
     </>
   );
