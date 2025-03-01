e13d7136b28c30cac5074e69e0af7de575d0177d
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-03-01 17:31:55 +0530

    🚨 important(analytics): add next-analytics for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-
+import { Analytics } from "@vercel/analytics/react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -43,7 +43,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster/>
+        <Toaster />
+        <Analytics />
         {children}
       </body>
     </html>
