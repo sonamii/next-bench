0893953e7dbadeb46e8d24213e513adf64a9c4f1
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-03-02 03:28:17 +0530

    📁 og(update): update the open-graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,14 +23,14 @@ export const metadata: Metadata = {
     title: "Next Bench by Sonamii",
     description:
       "Next Bench is a user-friendly web application that helps students to find the best institutions, offering advanced search and school comparisons.",
-    images: ["/og2.png"], // Assuming the image is at /public/thumbnail.png
+    images: ["/og.png"], // Assuming the image is at /public/thumbnail.png
   },
   twitter: {
     card: "summary_large_image",
     title: "Next Bench by Sonamii",
     description:
       "Next Bench is a user-friendly web application that helps students to find the best institutions, offering advanced search and school comparisons.",
-    images: ["/og2.png"],
+    images: ["/og.png"],
   },
 };
 
