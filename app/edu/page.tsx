f848bda094f9491d3ad6c9965e138b69ac66faaa
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-03-05 22:33:52 +0530

    🎨 design(explore): add search functionality with input

--- app/edu/page.tsx
+++ app/edu/page.tsx
@@ -1,7 +1,10 @@
 import { Navbar } from "@/custom/Nav";
 import { Footer } from "@/custom/footer";
 import { Table } from "./table2";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import "./style.css";
+import { Filter, FilterIcon } from "lucide-react";
 
 import { Component } from "./table";
 import { BookHeadphonesIcon, School, University } from "lucide-react";
@@ -13,22 +16,45 @@ export default function Explore() {
         <Navbar />
         <div className="search">
           <div className="sideBar">
-            <div className="item">
-              {/* <School /> */}
-              S
-            </div>
-            <div className="item">
-              {/* <University /> */}
-              U
-            </div>
-            <div className="item">
-              {/* <BookHeadphonesIcon size={20}/> */}
-              T
-            </div>
+            <div className="item">{/* <School /> */}S</div>
+            <div className="item">{/* <University /> */}U</div>
+            <div className="item">{/* <BookHeadphonesIcon size={20}/> */}</div>
             <div className="item"></div>
           </div>
           <div className="searchMain">
-            <Table></Table>
+            <div className="container">
+              <div className="heading">
+                <div className="text"> Search:</div>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "30px",
+                    width: "400px",
+                    justifyContent: "end",
+                    flexDirection: "row",
+                  }}
+                >
+                  <Input placeholder="Latest in your area..." type="text" />
+                  <Button>
+                    <FilterIcon size={26} />
+                  </Button>
+                </div>
+              </div>
+              <div className="filters">{/*to do*/}</div>
+              <div className="tableContainer">
+                <div className="attributes"></div>
+                <div className="tuples"></div>
+                <div className="tuples"></div>
+
+                <div className="tuples"></div>
+                <div className="tuples"></div>
+                <div className="tuples"></div>
+                <div className="tuples"></div>
+                <div className="tuples"></div>
+                <div className="tuples"></div>
+              </div>
+            </div>{" "}
           </div>
           <div className="profileCard"></div>
         </div>
--- app/edu/table2.tsx
+++ app/edu/table2.tsx
@@ -10,30 +10,5 @@ import {
 } from "@/components/ui/select";
 import { Filter, FilterIcon } from "lucide-react";
 export function Table() {
-  return (
-    <>
-      <div className="container">
-        <div className="heading">
-          <div className="text"> Search:</div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "30px",
-              width: "400px",
-              justifyContent: "end",
-              flexDirection: "row",
-            }}
-          >
-            <Input placeholder="Latest in your area..." type="text" />
-            <Button>
-              <FilterIcon size={26} />
-            </Button>
-          </div>
-        </div>
-        <div className="filters">{/*to do*/}</div>
-        <div className="tableContainer"></div>
-      </div>
-    </>
-  );
+  return <></>;
 }
