270fa29328b297b58f987f1aef7192cc3a9f43a1
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-03-08 21:31:34 +0530

    🎨 design(components): add more components to site

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,25 @@ import {
   BarChartBigIcon,
   Bot,
   Calendar,
+  Currency,
+  DonutIcon,
+  Figma,
+  Gitlab,
   GitMergeIcon,
   Grid2X2Icon,
- 
   LucideCircuitBoard,
   Luggage,
+  MessageCircle,
   Rotate3DIcon,
+  Search,
   Shapes,
   Shield,
+  ShieldQuestion,
+  Slack,
+  SparklesIcon,
   Star,
   UsersRound,
+  WandIcon,
   Wifi,
 } from "lucide-react";
 import "./style.css";
@@ -39,7 +48,6 @@ export default function Waitlist() {
         <div className="logo fade-item">
           <Image src="./logoMain.svg" alt="Logo" width={25} height={25} />
         </div>
-
         <div className="space-s"></div>
         <div className="members fade-item">
           <Image
@@ -51,9 +59,10 @@ export default function Waitlist() {
           />
           50+ People Joined
           <Link href="/waitlist">
-          <div className="button">
-            Join waitlist <ArrowRight size={14} style={{ marginLeft: "5px" }} />
-          </div>
+            <div className="button">
+              Join waitlist{" "}
+              <ArrowRight size={14} style={{ marginLeft: "5px" }} />
+            </div>
           </Link>
         </div>
         <div className="space-xs"></div>
@@ -62,10 +71,9 @@ export default function Waitlist() {
         </div>
         <div className="space-xs"></div>
         <div className="textBottom fade-item">
-          Finding the right school should be easy. Whether it&apos;s K-12, college,
-          or grad school, we simplify your search.
+          Finding the right school should be easy. Whether it&apos;s K-12,
+          college, or grad school, we simplify your search.
         </div>
-
         <div className="space-s"></div>
         <form
           className="inputContainer  fade-item"
@@ -82,6 +90,7 @@ export default function Waitlist() {
         </div>
         <div className="space"></div>
         <div className="space"></div>
+        {/*TOP FEATURES START */}
         <div className="topFeatures fade-item">
           <div className="left">
             <div className="badge fade-item">
@@ -125,8 +134,8 @@ export default function Waitlist() {
               </div>
               <div className="textBottom3">
                 Effortlessly find the perfect K-12 school, college, or graduate
-                program with our intuitive search system. Just enter a school&apos;s
-                name or locality to explore options.
+                program with our intuitive search system. Just enter a
+                school&apos;s name or locality to explore options.
               </div>
             </div>
 
@@ -143,7 +152,18 @@ export default function Waitlist() {
                 standout applications, and excel in extracurricular activities.
               </div>
             </div>
-
+            <div className="topFeaturesDisplay fade-item">
+              <div className="top">
+                <div className="topLogo">
+                  <Currency />
+                </div>
+                Admission Tracker & Comparison
+              </div>
+              <div className="textBottom3">
+                Keep track of your applications to schools, tuitions and compare
+                shortlisted schools to make informed decisions with ease.
+              </div>
+            </div>
             <div className="topFeaturesDisplay fade-item">
               <div className="top">
                 <div className="topLogo">
@@ -170,15 +190,204 @@ export default function Waitlist() {
                 is always available to help you with any questions or concerns
               </div>
             </div>
+            <div className="space"></div>
+            <div className="space"></div>
           </div>
         </div>
+        {/* TOP FEATURES END*/}
+        <div className="space"></div>
+        {/* WHY US START*/}
+        <div className="topFeatures topFeatures2 fade-item">
+          <div className="left">
+            <div className="badge fade-item">
+              <SparklesIcon size={15} style={{ marginRight: "5px" }} />
+              Why Next Bench?
+            </div>
+            <div className="textTop2 fade-item">
+              Why should you choose next bench?{" "}
+            </div>
+            <div className="textBottom3 fade-item">
+              Your One-Stop Platform for Finding Schools, Tuitions, and Teaching
+              Opportunities All At ONE PLACE.
+            </div>
+            <div className="space-xs"></div>
+            <div className="userContainer fade-item">
+              <div className="item">
+                <div className="text">200+</div>
+                <div className="bottomText">User Views</div>
+              </div>
+              <div className="item bordered">
+                <div className="text">10+</div>
+                <div className="bottomText">Active Users</div>
+              </div>
+              <div className="item">
+                <div className="text">4.7</div>
+                <div className="bottomText">Rating</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="right right2">
+            <div className="whyUsDisplay fade-item">
+              <div className="top">
+                <div className="topLogo">
+                  <Search />
+                </div>
+                Easy search{" "}
+              </div>
+              <div className="textBottom4">
+                Find top schools, tuitions, and tutors in your area quickly.
+              </div>
+            </div>
+            <div className="whyUsDisplay fade-item">
+              <div className="top">
+                <div className="topLogo">
+                  <Bot />
+                </div>
+                AI Guidance
+              </div>
+              <div className="textBottom4">
+                Get a personalized roadmap for success and other questions.
+              </div>
+            </div>
+            <div className="whyUsDisplay fade-item">
+              <div className="top">
+                <div className="topLogo">
+                  <DonutIcon />
+                </div>
+                Simple Application
+              </div>
+              <div className="textBottom4">
+                Apply for tuitions, and teaching jobs hassle-free in one click.
+              </div>
+            </div>
+            <div className="whyUsDisplay fade-item">
+              <div className="top">
+                <div className="topLogo">
+                  <Shield />
+                </div>
+                Trusted Network
+              </div>
+              <div className="textBottom4">
+                Connect ONLY with verified institutions and best tutors.
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* WHY US END */}
         <div className="space"></div>
         <div className="space"></div>
+        {/*ABOUT US START */}
+        <div className="topFeatures topFeatures2 fade-item">
+          <div className="left">
+            <div className="badge fade-item">
+              <WandIcon size={15} style={{ marginRight: "5px" }} />
+              About Us
+            </div>
+            <div className="textTop2 fade-item">
+              Revolutionizing school searching{" "}
+            </div>
+            <div className="textBottom3 fade-item">
+              Next Bench, under Sonamii, is a startup which simplifies finding
+              and applying to schools, universities, institutions, and tuitions
+              in one click. You get all the important feature all in one place.
+              With Next-AI, our smart AI agent, users get personalized guidance
+              for admissions and profile building.
+            </div>
+            <div className="space-xxs"></div>
+            <a href={"/auth/login"}>
+              {" "}
+              <div className="button">Pre-Login Now</div>
+            </a>
+            <div className="space-xxs"></div>
+            <div className="userContainer userContainer2 fade-item">
+              <div className="item">
+                <div className="text">200+</div>
+                <div className="bottomText">User Views</div>
+              </div>
+              <div className="item bordered">
+                <div className="text">10+</div>
+                <div className="bottomText">Active Users</div>
+              </div>
+
+              <div className="item">
+                <div className="text">4</div>
+                <div className="bottomText">Team Members</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="right">
+            <div className="imageCont">
+              <Image
+                src={"/aboutUsImg.png"}
+                alt="Logo"
+                width={440}
+                height={440}
+              ></Image>
+            </div>
+          </div>
+        </div>
+        {/* ABOUT US END*/}
         <div className="space"></div>
+        {/*SPONSERSHIPS START */}
+        <div className="topFeatures topFeatures2 fade-item">
+          <div className="left">
+            <div className="badge fade-item">
+              <Slack size={15} style={{ marginRight: "5px" }} />
+              Partners
+            </div>
+
+            <div className="textTop2  fade-item">
+              Explore Our Trusted Partnerships{" "}
+            </div>
+            <div className="textBottom3 fade-item">
+              Next Bench is backed by leading companies and institutions. Our
+              trusted partners help us provide a seamless and secure experience
+              for students, parents, and educators.
+            </div>
+          </div>
+
+          <div className="right textPartnership">
+            <div className="partnersDisplay">
+              <Figma />
+            </div>{" "}
+            <div className="partnersDisplay">
+              <Gitlab />
+            </div>
+          </div>
+        </div>
+        {/*SPONSERSHIPS END */}
+        <div className="space"></div> <div className="space"></div>
+        {/*TESTIMONIALS START */}
+        {/* <div className="topFeatures topFeatures2 fade-item">
+          <div className="left">
+            <div className="badge fade-item">
+              <MessageCircle size={15} style={{ marginRight: "5px" }} />
+              Testimonials
+            </div>
+
+            <div className="textTop2  fade-item">Coming Soon </div>
+          </div>
+        </div> */}
+        {/*TESTIMONIALS END */}
+        {/*FAQS START */}
+        {/* <div className="topFeatures topFeatures2 fade-item">
+          <div className="left">
+            <div className="badge fade-item">
+              <ShieldQuestion size={15} style={{ marginRight: "5px" }} />
+              FAQs
+            </div>
+            <div className="textTop2  fade-item">Coming Soon </div>
+          </div>
+        </div> */}
+        {/*FAQS END */}
         <div className="space"></div>
-        <div className="space"></div>
-        <div className="space"></div>
-        <div className="space"></div>
+        <div className="footer">
+          <div className="left"></div>
+          <div className="right"></div>
+        </div>
+        <div className="space-xs"></div>
       </div>
     </>
   );
