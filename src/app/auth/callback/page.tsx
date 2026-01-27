ccff21e90add87611bf57b852db2d296383c7c5f
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2026-01-27 18:16:10 +0530

    💄ui: add particle variation

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -31,6 +31,7 @@ import {
 import Image from "next/image";
 import { companyLogo } from "@/resources/next-bench.config";
 import supabase from "@/app/supabase/client";
+import { v4 as uuidv4 } from "uuid";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -50,9 +51,18 @@ export default function Callback() {
     checkSession();
   }, [router]);
 
-
   return (
     <Flex fillWidth fillHeight style={{ minHeight: "100vh" }} center>
+      <Particle
+        fill
+        style={{
+          width: "100vw",
+          height: "100vh",
+          position: "absolute",
+          zIndex: -1,
+        }}
+        speed={2}
+      />
       <Spinner size="l" />
     </Flex>
   );
